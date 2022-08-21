535fbe910f70b479fbc607d234a3f57fd91d38e4
Cody Allen <AlienWrench46@gmail.com>
2022-08-20 20:06:14 -0600

    Finish first 3 TDD exercises.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverseWord = '';
+
+    for (i = 1; i <= string.length; i++) {
+        reverseWord += string.substr(-(i), 1);
+    }
+
+    return reverseWord;
 
 };
 
