b9558871533452f39d30dd59e596ff51c66e5eb7
Cody Allen <AlienWrench46@gmail.com>
2022-08-22 03:14:00 -0600

    Finish 7th TOP TDD exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  temp = (temp - 32) * (5/9);
+  temp = temp.toFixed(1);
 
+  return Number(temp);
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  temp = temp * (9/5) + 32;
+  temp = temp.toFixed(1);
 
+  return Number(temp);
 };
 
 // Do not edit below this line
