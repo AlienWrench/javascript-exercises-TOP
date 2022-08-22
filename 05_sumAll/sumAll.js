c2e1cddd8d2e3e5869b146159e3e0d08c6bf7a71
Cody Allen <AlienWrench46@gmail.com>
2022-08-22 02:38:26 -0600

    Finish 5th TOP TDD exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if (typeof start !== 'number' || typeof end !== 'number' || start + end <= 0) {
+        return "ERROR";
+    } else if (start < end) {
+        let i = start;
+        while (i <= end) {
+            sum += i;
+            i++;
+        }
+        return sum;
+    } else if (end < start) {
+        let i = end;
+        while (i <= start) {
+            sum += i;
+            i++;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
