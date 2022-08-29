7002167785cb989d0691be01aa0a1115f3e252dc
Cody Allen <AlienWrench46@gmail.com>
2022-08-29 05:29:01 -0600

    Finish 10th TOP javascript exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const startingValue1 = 1;
+    const startingValue2 = 1;
+    const fibonacciArray = [startingValue1, startingValue2];
 
+    if (num > 0) {
+        let i = 0;
+        let j = 1; //index positions
+        while (fibonacciArray.length < num) {
+            fibonacciArray.push(fibonacciArray[i] + fibonacciArray[j]);
+            i++;
+            j++;
+        };
+
+        return fibonacciArray[num - 1];
+    } 
+    return "OOPS";
 };
 
 // Do not edit below this line
