cb4364cb557d07c413004ed7f62256ec0bde5d4b
Cody Allen <AlienWrench46@gmail.com>
2022-08-29 05:05:38 -0600

    Finish 9th TOP javascript exercise.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let reverseCounter;
+    string = string.toLowerCase();
 
-};
+    for (let j = 0; j < string.length; j++) {
+        if (string.charAt(j) === ' ' || string.charAt(j) === '!' || string.charAt(j) === ',' || string.charAt(j) === '.' ) {
+            string = string.replace(string.charAt(j), '');
+            j--;
+        }
+    }
+
+    reverseCounter = string.length - 1;
+
+    for (let i = 0; i <string.length; i++) {
+        if (string.charAt(i) !== string.charAt(reverseCounter)) {
+            return false;
+        }
+        reverseCounter--;
+    }
+
+    return true;
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
