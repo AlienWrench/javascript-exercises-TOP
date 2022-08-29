0421f34232e71e1bac6893127fa56d6b2405b87e
Cody Allen <AlienWrench46@gmail.com>
2022-08-29 03:33:02 -0600

    Finish 8th TOP javascript project.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(a, b) {
+  const solution = a + b;
+
+  return solution;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	const solution = a - b;
+
+  return solution;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let solution = 0;
+  for (let i = 0; i < array.length; i++) {
+    solution += array[i];
+  }
+
+  return solution;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let solution = 1;
+
+  for (let i = 0; i < array.length; i++) {
+    solution *= array[i];
+  }
+
+  return solution;
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  const solution = a ** b;
+	return solution;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let solution = 1;
+
+  if (num === 0 || num === 1) {
+
+  } else {
+    for (let i = num; i > 0; i--) {
+      solution *= i;
+    }
+  }
+
+  return solution;
 };
 
 // Do not edit below this line
